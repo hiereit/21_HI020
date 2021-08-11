57d497d2937422c70b768010aab21bc66bf5c40b
a-taeyeon <taeyeon0107@naver.com>
2021-08-12 00:40:47 +0900

    point return activity

---
+++ app/src/main/java/com/hanium/greenduks/PointReturnActivity.java
@@ -0,0 +1,15 @@
+package com.hanium.greenduks;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PointReturnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pointreturn);
+
+    }
+}
