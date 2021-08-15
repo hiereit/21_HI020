704ad3028453dc294d4aae187e0bb0047ca70447
a-taeyeon <taeyeon0107@naver.com>
2021-08-15 21:53:42 +0900

    add navigation menu to layouts

--- app/src/main/java/com/hanium/greenduks/QnaBoardActivity.java
+++ app/src/main/java/com/hanium/greenduks/QnaBoardActivity.java
@@ -1,14 +1,26 @@
 package com.hanium.greenduks;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class QnaBoardActivity extends AppCompatActivity {
+
+    ImageView iv_menu;
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qnaboard);
+
+        iv_menu = findViewById(R.id.iv_menu);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        iv_menu.setOnClickListener(v -> drawerLayout.openDrawer(Gravity.LEFT));
+
     }
 }
