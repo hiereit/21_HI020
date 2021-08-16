7f99906ab2f2fe9276f98134f84c9111b8ce0385
a-taeyeon <taeyeon0107@naver.com>
2021-08-17 00:05:11 +0900

    add license

--- app/src/main/java/com/hanium/greenduks/LicenseActivity.java
+++ app/src/main/java/com/hanium/greenduks/LicenseActivity.java
@@ -1,15 +1,56 @@
 package com.hanium.greenduks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class LicenseActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class LicenseActivity extends AppCompatActivity implements NavigationInterface, NavigationView.OnNavigationItemSelectedListener{
+
+    ImageView iv_menu;
+    DrawerLayout drawerLayout;
+    ImageView iv_qr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license);
 
+        iv_menu = findViewById(R.id.iv_menu);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        iv_menu.setOnClickListener(v -> drawerLayout.openDrawer(Gravity.LEFT));
+        TextView toolbar_name = findViewById(R.id.tvToolbar_name);
+
+        initializeLayout(iv_menu, drawerLayout, toolbar_name, "포인트 확인");
+        setNavigationViewListener();
+
+        iv_qr = findViewById(R.id.iv_qr);
+        iv_qr.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), QrScanActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d("yyj", "item: " + item);
+        Intent intent = nextIntent(item, this, drawerLayout);
+        startActivity(intent);
+        return true;
+    }
+
+    public void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
